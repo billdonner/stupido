9e6916680c9bc2b5b2efca6b373fef157a8e5273
bill donner <billdonner@gmail.com>
2023-07-12 10:31:45 -0500

    Initial Commit

---
+++ stupido/stupidoApp.swift
@@ -0,0 +1,17 @@
+//
+//  stupidoApp.swift
+//  stupido
+//
+//  Created by bill donner on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct stupidoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
